b7cf5cc17a092d58057ca93b40ec84e4ee79da22
7rans <transfire@gmail.com>
2010-05-09 22:37:13 -0400

    improved ANSI::Code, but no longer support string argument interface

--- lib/ansi/code.rb
+++ lib/ansi/code.rb
@@ -1,18 +1,6 @@
-# Ansi::Code Copyright (c) 2009 Thomas Sawyer, LGPL
-#
-# This library is a partial adaptation of ANSIColor by Florian Frank.
-#
-# ANSIColor Copyright (c) 2002 Florian Frank, LGPL
-#
-# TODO: Need to add rest of ANSI codes. Include modes?
-# TODO: Re-evaluate how color/yielding methods are defined.
-# TODO: Maybe up, down, right, left should have yielding methods too?
+require 'Win32/Console/ANSI' if RUBY_PLATFORM =~ /win32/
 
-#
 module ANSI
-
-  # Currently Windows is not supported.
-  SUPPORTED = !(RUBY_PLATFORM =~ /win/)
 
   # = ANSI Codes
   #
@@ -75,31 +63,144 @@ module ANSI
   #     on_cyan
   #     on_white
   #
-  module Code
+  # This library is a partial adaptation of ANSIColor by Florian Frank.
+  #
+  # ANSIColor Copyright (c) 2002 Florian Frank, LGPL
+  #
+  # TODO: Need to add rest of ANSI codes. Include modes?
+  # TODO: Re-evaluate how color/yielding methods are defined.
+  # TODO: Maybe up, down, right, left should have yielding methods too?
 
+  module Code
     extend self
+
+    CLEAR      = "\e[0m"
+    RESET      = "\e[0m"
+    BOLD       = "\e[1m"
+    DARK       = "\e[2m"
+    ITALIC     = "\e[3m"  # not widely implemented
+    UNDERLINE  = "\e[4m"
+    UNDERSCORE = "\e[4m"
+    BLINK      = "\e[5m"
+    RAPID      = "\e[6m"  # not widely implemented
+    REVERSE    = "\e[7m"
+    NEGATIVE   = "\e[7m"  # alternate to reverse because of String#reverse
+    CONCEALED  = "\e[8m"
+    STRIKE     = "\e[9m"  # not widely implemented
+
+    BLACK      = "\e[30m"
+    RED        = "\e[31m"
+    GREEN      = "\e[32m"
+    YELLOW     = "\e[33m"
+    BLUE       = "\e[34m"
+    MAGENTA    = "\e[35m"
+    CYAN       = "\e[36m"
+    WHITE      = "\e[37m"
+
+    ON_BLACK   = "\e[40m"
+    ON_RED     = "\e[41m"
+    ON_GREEN   = "\e[42m"
+    ON_YELLOW  = "\e[43m"
+    ON_BLUE    = "\e[44m"
+    ON_MAGENTA = "\e[45m"
+    ON_CYAN    = "\e[46m"
+    ON_WHITE   = "\e[47m"
+
+    # Save current cursor positon.
+    SAVE       = "\e[s"
+
+    # Restore saved cursor positon.
+    RESTORE    = "\e[u"
+
+    # Clear to the end of the current line.
+    CLEAR_LINE = "\e[K"
+
+    # Clear to the end of the current line.
+    CLR        = "\e[K"
+
+    # Clear the screen and move cursor to home.
+    CLEAR_SCREEN = "\e[2J"
+
+    # Clear the screen and move cursor to home.
+    CLS          = "\e[2J"
+
+    styles = %w{bold dark italic underline underscore blink rapid reverse negative concealed strike}
+
+    styles.each do |color|
+      module_eval %{
+        def #{color}
+          block_given? ? "\#{#{color.upcase}}\#{yield}\#{CLEAR}" : #{color.upcase}
+        end
+      }
+    end
+
+    colors = %w{black red green yellow blue magenta cyan white}
+
+    colors.each do |color|
+      module_eval %{
+        def #{color}
+          block_given? ? "\#{#{color.upcase}}\#{yield}\#{CLEAR}" : #{color.upcase}
+        end
+
+        def on_#{color}
+          block_given? ? "\#{ON_#{color.upcase}}\#{yield}\#{CLEAR}" : ON_#{color.upcase}
+        end
+      }
+    end
+
+    # Dynamically create color on color methods.
+
+    colors.each do |color|
+      colors.each do |on_color|
+        module_eval %{
+          def #{color}_on_#{on_color}
+            if block_given?
+              #{color.upcase} + ON_#{on_color.upcase} + yield.to_s + CLEAR
+            else
+              #{color.upcase} + ON_#{on_color.upcase}
+            end
+          end
+        }
+      end
+    end
+
+    def clear
+      CLEAR
+    end
+
+    def reset
+      RESET
+    end
 
     # Save current cursor positon.
     def save
-      "\e[s"
+      SAVE
     end
 
     # Restore saved cursor positon.
     def restore
-      "\e[u"
+      RESTORE
+    end
+
+    # Clear to the end of the current line.
+    def clear_line
+      CLEAR_LINE
+    end
+
+    # Clear to the end of the current line.
+    def clr
+      CLR
     end
 
     # Clear the screen and move cursor to home.
     def clear_screen
-      "\e[2J"
+      CLEAR_SCREEN
     end
-    alias_method :cls, :clear_screen
 
-    # Clear to the end of the current line.
-    def clear_line
-      "\e[K"
+    # Clear the screen and move cursor to home.
+    def cls
+      CLS
     end
-    alias_method :clr, :clear_line
 
     #--
     #def position
@@ -108,149 +209,154 @@ module ANSI
     #++
 
     # Move curose to line and column.
-    def move( line, column=0 )
+    def move(line, column=0)
       "\e[#{line.to_i};#{column.to_i}H"
     end
 
     # Move cursor up a specificed number of spaces.
-    def up( spaces=1 )
+    def up(spaces=1)
       "\e[#{spaces.to_i}A"
     end
 
     # Move cursor down a specificed number of spaces.
-    def down( spaces=1 )
+    def down(spaces=1)
       "\e[#{spaces.to_i}B"
     end
 
     # Move cursor left a specificed number of spaces.
-    def left( spaces=1 )
+    def left(spaces=1)
       "\e[#{spaces.to_i}D"
     end
 
     # Move cursor right a specificed number of spaces.
-    def right( spaces=1 )
+    def right(spaces=1)
       "\e[#{spaces.to_i}C"
     end
 
-    # Like +move+ but returns to original positon
-    # after yielding block or adding string argument.
-    def display( line, column=0, string=nil ) #:yield:
+    # Like +move+ but returns to original positon after
+    # yielding the block.
+    def display(line, column=0) #:yield:
       result = "\e[s"
       result << "\e[#{line.to_i};#{column.to_i}H"
       if block_given?
         result << yield
         result << "\e[u"
-      elsif string
-        result << string
-        result << "\e[u"
-      elsif respond_to?(:to_str)
-        result << self
-        result << "\e[u"
+      #elsif string
+      #  result << string
+      #  result << "\e[u"
+      #elsif respond_to?(:to_str)
+      #  result << self
+      #  result << "\e[u"
       end
-      return result
+      result
     end
-
-    # Define color codes.
-    def self.define_ansicolor_method(name,code)
-      class_eval <<-HERE
-        def #{name.to_s}(string = nil)
-            result = "\e[#{code}m"
-            if block_given?
-                result << yield
-                result << "\e[0m"
-            elsif string
-                result << string
-                result << "\e[0m"
-            elsif respond_to?(:to_str)
-                result << self
-                result << "\e[0m"
-            end
-            return result
-        end
-      HERE
-    end
-
-    @@colors = [
-      [ :clear        ,   0 ],
-      [ :reset        ,   0 ],     # synonym for :clear
-      [ :bold         ,   1 ],
-      [ :dark         ,   2 ],
-      [ :italic       ,   3 ],     # not widely implemented
-      [ :underline    ,   4 ],
-      [ :underscore   ,   4 ],     # synonym for :underline
-      [ :blink        ,   5 ],
-      [ :rapid_blink  ,   6 ],     # not widely implemented
-      [ :negative     ,   7 ],     # no reverse because of String#reverse
-      [ :concealed    ,   8 ],
-      [ :strikethrough,   9 ],     # not widely implemented
-      [ :black        ,  30 ],
-      [ :red          ,  31 ],
-      [ :green        ,  32 ],
-      [ :yellow       ,  33 ],
-      [ :blue         ,  34 ],
-      [ :magenta      ,  35 ],
-      [ :cyan         ,  36 ],
-      [ :white        ,  37 ],
-      [ :on_black     ,  40 ],
-      [ :on_red       ,  41 ],
-      [ :on_green     ,  42 ],
-      [ :on_yellow    ,  43 ],
-      [ :on_blue      ,  44 ],
-      [ :on_magenta   ,  45 ],
-      [ :on_cyan      ,  46 ],
-      [ :on_white     ,  47 ],
-    ]
-
-    @@colors.each do |c, v|
-      define_ansicolor_method(c, v)
-    end
-
-    ColoredRegexp = /\e\[([34][0-7]|[0-9])m/
-
-    module_function
 
     #
-    def uncolored(string = nil)
+    def style(*codes)
+      s = ""
+      codes.each do |code|
+        s << "\e[#{STYLES[code]}m"
+      end
+      s << yield
+      s << CLEAR
+    end
+
+    #
+    def unstyle
       if block_given?
-        yield.gsub(ColoredRegexp, '')
-      elsif string
-        string.gsub(ColoredRegexp, '')
-      elsif respond_to?(:to_str)
-        gsub(ColoredRegexp, '')
+        yield.gsub(PATTERN, '')
+      #elsif string
+      #  string.gsub(ColoredRegexp, '')
+      #elsif respond_to?(:to_str)
+      #  gsub(ColoredRegexp, '')
       else
         ''
       end
     end
 
+    # old term
+    alias_method :uncolored, :unstyle
+
+=begin
+    # Define color codes.
+    def self.define_ansicolor_method(name,code)
+      class_eval <<-HERE, __FILE__, __LINE__
+        def #{name.to_s}(string = nil)
+          result = "\e[#{code}m"
+          if block_given?
+            result << yield
+            result << "\e[0m"
+          elsif string
+            result << string
+            result << "\e[0m"
+          elsif respond_to?(:to_str)
+            result << self
+            result << "\e[0m"
+          end
+          return result
+        end
+      HERE
+    end
+=end
+
+    PATTERN = /\e\[([34][0-7]|[0-9])m/
+
+    STYLES = {
+      :clear        =>   0,
+      :reset        =>   0,
+      :bold         =>   1,
+      :dark         =>   2,
+      :italic       =>   3,
+      :underline    =>   4,
+      :underscore   =>   4,
+      :blink        =>   5,
+      :rapid        =>   6,
+      :reverse      =>   7,
+      :negative     =>   7,
+      :concealed    =>   8,
+      :strike       =>   9,
+      :black        =>  30,
+      :red          =>  31,
+      :green        =>  32,
+      :yellow       =>  33,
+      :blue         =>  34,
+      :magenta      =>  35,
+      :cyan         =>  36,
+      :white        =>  37,
+      :on_black     =>  40,
+      :on_red       =>  41,
+      :on_green     =>  42,
+      :on_yellow    =>  43,
+      :on_blue      =>  44,
+      :on_magenta   =>  45,
+      :on_cyan      =>  46,
+      :on_white     =>  47
+    }
+
+=begin
+    @@colors.each do |c, v|
+      define_ansicolor_method(c, v)
+    end
+
+
     #
     def colors
-      @@colors.map { |c| c[0] }
+      @@colors.map{ |c| c[0] }
     end
+
+=end
 
   end
 
-  # ANSI::Functor is used to extend String.
-  class Functor
-
-    #
-    def initialize(string)
-      @string = string
-    end
-
-    #
-    def method_missing(op, *args, &block)
-      ANSI::Code.send(op, @string, *args, &block)
-    end
-
-  end
+  extend Code
 
 end
 
 #
 class String
   #
-  def ansi
-    ANSI::Functor.new(self)
+  def ansi(*codes)
+    ANSI::Code.style(*codes){ self }
   end
 end
 
