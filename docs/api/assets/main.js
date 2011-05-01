70c64845299b303b872932a3a1c3b61731380aab
7rans <transfire@gmail.com>
2011-05-01 10:33:52 -0400

    Updated files.

--- docs/api/assets/main.js
+++ docs/api/assets/main.js
@@ -54,12 +54,12 @@ function hookDebuggingToggle() {
 
 function hookQuickSearch() {
 	$('.quicksearch-field').each( function() {
-		var searchElems = $(this).parents('.section').find( 'li' );
+		var searchElems = $(this).parents('.section').find('li');
 		var toggle = $(this).parents('.section').find('h3 .search-toggle');
 		// console.debug( "Toggle is: %o", toggle );
 		var qsbox = $(this).parents('form').get( 0 );
 		
-		$(this).quicksearch( this, searchElems, {
+		$(this).quicksearch(this, searchElems, {
 			noSearchResultsIndicator: 'no-class-search-results',
 			focusOnLoad: false
 		});
