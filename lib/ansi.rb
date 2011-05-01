8363e7de6ea1bc2f22c64bb0f91810072999a1be
7rans <transfire@gmail.com>
2011-05-01 10:14:52 -0400

    admin: update metadata

--- lib/ansi.rb
+++ lib/ansi.rb
@@ -1,8 +1,18 @@
 # ANSI module contains all the ANSI related classes.
 module ANSI
+  #
+  def self.meta
+    @spec ||= (
+      require 'yaml'
+      YAML.load(File.new(File.dirname(__FILE__) + '/ansi.yml'))
+    )
+  end
+  #
+  def self.const_missing(name)
+    meta[name.to_s.downcase] || super(name)
+  end
 end
 
-require 'ansi/version'
 require 'ansi/code'
 require 'ansi/logger'
 require 'ansi/progressbar'
